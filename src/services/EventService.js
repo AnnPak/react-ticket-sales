db6a7d8cb392b61f2c113e4904054d227d203663
Anna <anna_p@rupor.agency>
2022-09-15 14:06:44 +0300

    created useEventServise hook

---
+++ src/services/EventService.js
@@ -0,0 +1,28 @@
+import {useHttp} from '../hooks/http.hook';
+
+
+
+export const useEventServise = () => {
+
+    const {request} = useHttp();
+
+    const getEvent = async (id) => {
+        const res = await request(`https://events.free.beeceptor.com`);
+        const newArr = res.events.filter(el => el.id == id)
+
+        return _transformEvents(newArr[0]);
+    }
+
+    const _transformEvents = (event) => {
+        
+        return {
+            id: event.id,
+            title: event.title,
+            description: event.description ? `${event.description.slice(0, 210)}...` : 'There is no description for this character',
+        }
+    }
+
+    return {
+        getEvent
+    }
+}
